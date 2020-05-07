663bea6b992481dc180f7a68faa446271226ee1a
hi <hi@DESKTOP-G463TQN>
2020-05-07 11:16:57 +0900

    hello

---
+++ src/com/javaex/practice/Helloworld.java
@@ -0,0 +1,12 @@
+package com.javaex.practice;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world!");
+
+	}
+
+}
